09579a00df862ceb20e57c11c53731c623d0f8fd
cypher <lutalidavid44@gmail.com>
2022-12-28 10:55:47 +0300

    incomplete

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@ public class  Main {
     public static void main(String[] args) {
 addmovie addmovie =new addmovie();
 addmovie.createMovie();
+customerUi customerUi = new customerUi();
+customerUi.createCustomer();
+login login =new login();
+login.createLogin();
+payment payment = new payment();
+payment.createPayment();
+register register = new register();
+register.createRegister();
+cancelpayment cancelpayment = new cancelpayment();
+cancelpayment.createCancel();
     }
 }
